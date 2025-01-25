49e0d695a1f08d8429cc9ad7195256d6e3fb9fe2
Truong Nguyen <vonguyen.0407@gmail.com>
2025-01-25 21:57:46 +0700

    make seeder demo in user

---
+++ src/Command/SeedUsersCommand.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Command;
+
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Doctrine\ORM\EntityManagerInterface;
+use Faker\Factory as FakerFactory;
+use App\Entity\User;
+
+#[AsCommand(
+    name: 'app:seed-users',
+    description: 'Add a short description for your command',
+)]
+class SeedUsersCommand extends Command
+{
+    protected static $defaultName = 'app:seed-users';
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $faker = FakerFactory::create();
+        
+        for ($i = 1; $i <= 10; $i++) {
+            $user = new User();
+            $user->setFirstname($faker->firstName);
+            $user->setLastname($faker->lastName);
+            $user->setEmail($faker->unique()->email);
+            $user->setPassword(password_hash('password', PASSWORD_BCRYPT)); // Example hashed password
+            
+            $this->entityManager->persist($user);
+
+            $output->writeln("Created user: {$user->getFirstname()} {$user->getLastname()}");
+        }
+
+        $this->entityManager->flush();
+        $output->writeln('Seeding complete!');
+
+        return Command::SUCCESS;
+    }
+}
