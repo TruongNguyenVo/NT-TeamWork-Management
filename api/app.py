379bf2d8b37dbb372a081fc392e0a1c156ba9311
Truong Nguyen <vonguyen.0407@gmail.com>
2025-02-15 19:21:03 +0700

    create run.bat to run both symfony and flask

--- api/app.py
+++ api/app.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def predict():
+    data = {
+        "status": 404,
+        "message": "not found"
+    }
+    
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- run.bat
+++ run.bat
@@ -2,5 +2,5 @@
 start /B symfony server:start --port=8386
 
 cd api
-source venv/bin/activate
+source penv/Scripts/activate
 python app.py
