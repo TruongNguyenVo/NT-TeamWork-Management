0776d26399086a3280221e87687591a00efc6527
Truong Nguyen <vonguyen.0407@gmail.com>
2025-02-25 23:38:44 +0700

    áp được model rồi mà model lỏ quáaaaaa không thấm nổinổi

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ import connectDatabase
 app = Flask(__name__)
 
 # Load model
-with open('models\\decision_tree_model.pkl', 'rb') as f:
+with open('models\\decision_tree_classificant_but_result_is_percent.pkl', 'rb') as f:
     model = joblib.load(f)
 
 @app.route('/predict', methods=['GET', 'POST'])
@@ -22,10 +22,11 @@ def predict():
         values.extend(val) # append the value (flatten the list)
 
     values_reshaped = values
-    predictions = model.predict(values_reshaped) # predict the values
+    predictions = model.predict_proba(values_reshaped) # predict the values
+    predictions = [round(pred[1] * 100, 2) for pred in predictions] # convert to percentage and round to 2 decimal places
 
     for i, key in enumerate(names):
-        respondData[key] = predictions[i] # add to dictionary
+        respondData[key] = str(predictions[i]) + "%" # add to dictionary
     
     return jsonify(respondData)
 
